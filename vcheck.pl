5d377d213d2a3c92f5a14b4df19273cc68fc9d1d
Kent Fredric <kentnl@gentoo.org>
2020-10-27 08:15:47 +1300

    Add my working but hacky-as-fuck tooling

---
+++ vcheck.pl
@@ -0,0 +1,148 @@
+#!perl
+use strict;
+use warnings;
+
+use Test::TempDir::Tiny qw( in_tempdir );
+use Capture::Tiny qw( capture );
+
+our $CRATE = "gcc";
+
+our $VERSION_BASE        = "/home/kent/rust/vmatrix";
+our $VERSION_SOURCE      = "${VERSION_BASE}/${CRATE}/versions.txt";
+our $VERSION_DEST_PREFIX = "${VERSION_BASE}/${CRATE}/rustc-";
+
+our (@RUSTC) = (
+    qw(
+      1.0.0  1.1.0  1.2.0  1.3.0  1.4.0  1.5.0  1.6.0  1.7.0  1.8.0  1.9.0
+      1.10.0 1.11.0 1.12.1 1.13.0 1.14.0 1.15.1 1.16.0 1.17.0 1.18.0 1.19.0
+      1.20.0 1.21.0 1.22.1 1.23.0 1.24.1 1.25.0 1.26.2 1.27.2 1.28.0 1.29.2
+      1.30.1 1.31.1 1.32.0 1.33.0 1.34.2 1.35.0 1.36.0 1.37.0 1.38.0 1.39.0
+      1.40.0 1.41.1 1.42.0 1.43.1 1.44.1 1.45.2 1.46.0 1.47.0
+      )
+);
+for my $rustc (@RUSTC) {
+    if ( -e "${VERSION_DEST_PREFIX}${rustc}" ) {
+        warn "Results found for $CRATE on rustc-$rustc, skipping";
+        next;
+    }
+    in_tempdir "$CRATE-rustc$rustc" => sub {
+        my $cwd = shift;
+        my (@versions) = get_versions($VERSION_SOURCE);
+
+        printf "entering %s\n", $cwd;
+
+        do_testset(
+            crate           => $CRATE,
+            versions        => \@versions,
+            rustc_version   => $rustc,
+            rustc_toolchain => $rustc,
+            work_dir        => $cwd,
+            result_file     => $VERSION_DEST_PREFIX . $rustc,
+        );
+    };
+}
+
+sub get_versions {
+    my ($path) = @_;
+    open my $fh, "<", $path or die "can't read $path";
+    my @v;
+    while ( my $line = <$fh> ) {
+        chomp $line;
+        my ( $version, $message, @rest ) = split /[|]/, $line;
+        my $rec = { version => $version };
+        if ( defined $message and length $message ) {
+            $rec->{message} = $message;
+        }
+        if (@rest) {
+            $rec->{extras} = \@rest;
+        }
+        push @v, $rec;
+    }
+    return @v;
+}
+
+sub do_test {
+    my (%params) = @_;
+
+    my $rustc_version   = $params{rustc_version};
+    my $rustc_toolchain = $params{rustc_toolchain};
+
+    my $crate   = $params{crate};
+    my $version = $params{version};
+
+    my $work_dir = $params{work_dir};
+
+    my $cargo_toml = "${work_dir}/Cargo.toml";
+    my $src_dir    = "${work_dir}/src";
+    my $lib_path   = "${src_dir}/lib.rs";
+    {
+        open my $fh, ">", $cargo_toml or die "Can't write $cargo_toml";
+        $fh->print(<<"EOF");
+[package]
+name = "test"
+version = "0.1.0"
+authors = [ "Kent Fredric <kentnl\@gentoo.org>" ]
+
+[dependencies.${crate}]
+version = "=${version}"
+EOF
+        close $fh or warn "Error closing $cargo_toml, $!";
+    }
+    if ( !-d "${src_dir}" ) {
+        system( "mkdir", "-p", "${src_dir}" );
+    }
+    if ( !-e "${lib_path}" ) {
+        open my $fh, ">", $lib_path or die "Can't write $lib_path";
+        $fh->print("");
+        close $fh or warn "Error closing $lib_path, $!";
+    }
+    my $exit =
+      system( "/home/kent/bin/cargo.sh", "+${rustc_toolchain}", "build" );
+    if ( $exit != 0 ) {
+        printf "rustc %s w/ %s version %s \e[31mfail\e[0m\n", $rustc_version,
+          $crate, $version;
+        return undef;
+    }
+    printf "rustc %s w/ %s version %s \e[32mpass\e[0m\n", $rustc_version,
+      $crate, $version;
+    return 1;
+}
+
+sub do_testset {
+    my (%params)        = @_;
+    my $crate           = $params{crate};
+    my $versions        = $params{versions};
+    my $rustc_version   = $params{rustc_version};
+    my $rustc_toolchain = $params{rustc_toolchain};
+    my $work_dir        = $params{work_dir};
+    my $result_file     = $params{result_file};
+
+    my (@results);
+    for my $version ( reverse @$versions ) {
+        if ( not defined $version->{version} ) {
+            require Data::Dump;
+            die Data::Dump::pp($version);
+        }
+        if ( exists $version->{message} ) {
+            warn
+              "Skipping: $crate v$version->{version} : $version->{message}\n";
+            push @results, $version;
+            next;
+        }
+        my $result = do_test(
+            rustc_version   => $rustc_version,
+            rustc_toolchain => $rustc_toolchain,
+            crate           => $crate,
+            version         => $version->{version},
+            work_dir        => $work_dir,
+        );
+        $version->{message} = $result ? "pass" : "fail";
+        push @results, $version;
+    }
+    open my $fh, ">", $result_file or die "Can't write $result_file";
+    for my $result ( reverse @results ) {
+        $fh->printf( "%s|%s\n", $result->{version}, $result->{message} );
+    }
+    close $fh or warn "Error closing $result_file, $!\n";
+}
+
---
+++ vmatrix_gen.pl
@@ -0,0 +1,117 @@
+#!perl
+use strict;
+use warnings;
+
+our $ROOT        = "/home/kent/rust/vmatrix";
+our $CRATE       = "gcc";
+our $VINDEX      = "versions.txt";
+our $INFO_PREFIX = "rustc-";
+
+my %versions;
+my %rustc_results;
+my (@order);
+
+for my $version ( get_versions("${ROOT}/${CRATE}/${VINDEX}") ) {
+    next
+      if exists $version->{message}
+      and $version->{message}
+      and $version->{message} eq 'NOEXIST';
+    next
+      if exists $version->{message}
+      and $version->{message}
+      and $version->{message} eq 'YANKED';
+    my $v = $version->{version};
+    push @order, $v;
+    $versions{$v} = {} unless exists $versions{$v};
+}
+{
+    opendir my $dfh, "${ROOT}/${CRATE}"
+      or die "Can't opendir ${ROOT}/${CRATE}, $!";
+    while ( my $ent = readdir $dfh ) {
+        next if $ent eq '.';
+        next if $ent eq '..';
+        next unless $ent =~ /\A\Q$INFO_PREFIX\E(\d+.\d+.\d+)\z/;
+        my $rustc = $1;
+        my (@results) = get_versions("${ROOT}/${CRATE}/$ent");
+        $rustc_results{$rustc} = {} unless exists $rustc_results{$rustc};
+        for my $version (@results) {
+            my $v = $version->{version};
+            next unless exists $versions{$v};
+            $rustc_results{$rustc}{$v} = $version->{message};
+        }
+    }
+}
+my (@rustc_order) = sort { vsort( $a, $b ) } keys %rustc_results;
+
+# Create a rotated heading, lmao
+my $vspace = 0;
+for (@rustc_order) {
+    $vspace = length $_ if $vspace < length $_;
+}
+my $padline = ( " " x 10 ) . ( " " x scalar @rustc_order );
+my (@vpad)  = map { $padline } 0 .. $vspace;
+my $index   = 0;
+for (@rustc_order) {
+    my (@chars) = split //, $_;
+    my $row_index = $vspace;
+    for my $char ( reverse @chars ) {
+        substr $vpad[$row_index], $index + 10, 1, $char;
+        $row_index--;
+    }
+    $index++;
+}
+for (@vpad) {
+    printf "%s\n", $_;
+}
+
+for my $version ( reverse @order ) {
+    printf "%8s: ", $version;
+    for my $rustc (@rustc_order) {
+        if ( not exists $rustc_results{$rustc}{$version} ) {
+            print "?";
+            next;
+        }
+        if ( $rustc_results{$rustc}{$version} eq 'pass' ) {
+            print "\e[32;1m*\e[0m";
+            next;
+        }
+        if ( $rustc_results{$rustc}{$version} eq 'fail' ) {
+            print "\e[31m_\e[0m";
+            next;
+        }
+    }
+    print "\n";
+}
+
+sub get_versions {
+    my ($path) = @_;
+    open my $fh, "<", $path or die "can't read $path";
+    my @v;
+    while ( my $line = <$fh> ) {
+        chomp $line;
+        my ( $version, $message, @rest ) = split /[|]/, $line;
+        my $rec = { version => $version };
+        if ( defined $message and length $message ) {
+            $rec->{message} = $message;
+        }
+        if (@rest) {
+            $rec->{extras} = \@rest;
+        }
+        push @v, $rec;
+    }
+    return @v;
+}
+
+sub vsplit {
+    split /[.]/;
+}
+
+sub vsort {
+    my ( $lhs, $rhs ) = @_;
+    my (@lhs_parts) = split /[.]/, $lhs;
+    my (@rhs_parts) = split /[.]/, $rhs;
+
+    $lhs_parts[0] <=> $rhs_parts[0]
+      or $lhs_parts[1] <=> $rhs_parts[1]
+      or $lhs_parts[2] <=> $rhs_parts[2];
+}
