496f6f2d50496b2106064ce7b64c571debaebf96
Kent Fredric <kentnl@gentoo.org>
2020-10-28 19:51:21 +1300

    Harvest fail/pass data and give grading on the overview

--- gen_index.pl
+++ gen_index.pl
@@ -20,6 +20,30 @@ sub find_crates {
     return @crates;
 }
 
+sub grade_crate {
+    my ($crate) = @_;
+    my $cratedir = "${VERSION_BASE}/${crate}";
+    my ( $npass, $nfail ) = ( 0, 0 );
+    opendir my $dfh, $cratedir or die "Can't opendir $cratedir";
+    while ( my $ent = readdir $dfh ) {
+        next if $ent eq '.';
+        next if $ent eq '..';
+        next unless $ent =~ m/\Arustc-\d+[.]\d+[.]\d+\z/;
+        next if -d $ent;
+        my $file = "$cratedir/$ent";
+        open my $fh, "<", "$file" or die "Can't read $file, $!";
+        while ( my $line = <$fh> ) {
+            chomp $line;
+            my ( $version, $grade, @extra ) = split /[|]/, $line;
+            $npass++ if $grade eq 'pass';
+            $nfail++ if $grade eq 'fail';
+        }
+    }
+    return -1 if $npass > 1 and $nfail == 0;
+    my $pct_fail = sprintf "%0.1f", $nfail / ( $npass + $nfail );
+    return $pct_fail;
+}
+
 sub gen_toc {
     my %crate_buckets;
     for my $crate (find_crates) {
@@ -38,8 +62,21 @@ sub gen_toc {
             $bucket, $bucket, $bucket );
         $fh->printf("$pad<ul>\n");
         for my $crate ( sort @{ $crate_buckets{$bucket} } ) {
-            $fh->printf( "$pad  <li><a href=\"./%s\">%s</a></li>\n",
-                $crate, $crate );
+            $fh->printf( "$pad  <li><a href=\"./%s\">%s</a>", $crate, $crate );
+            my $grade = grade_crate($crate);
+            if ( $grade < 0 ) {
+                $fh->print(
+"<span class=\"grade goldstar\" title=\"No reported failures for any version on any rust\">&#x1F31F;</span>"
+                );
+            }
+            else {
+                $fh->printf(
+"<span class=\"grade numeric_%d\" title=\"%d%% failures\"></span>",
+                    $grade * 10,
+                    $grade * 100
+                );
+            }
+            $fh->print("</li>\n");
         }
         $fh->printf("$pad</ul>\n");
     }
