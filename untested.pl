72112db0f01249786ac51186ef83104488c4fb49
Kent Fredric <kentnl@gentoo.org>
2020-11-04 14:59:41 +1300

    untested: show mtime in mins field

--- untested.pl
+++ untested.pl
@@ -8,10 +8,12 @@ use resultdb;
 
 my $rdb = resultdb->new();
 for my $crate ( $rdb->crate_names ) {
+    my $vjs  = $rdb->crate_vjson_path($crate);
     my $info = $rdb->crate_info($crate);
     next if @{ $info->rustcs };
     my (@versions) = ( @{ $info->versions } );
     next unless @versions;
     my (@deps) = ( keys %{ $info->dependencies } );
-    printf "%d %d %s\n", scalar @versions, scalar @deps, $crate;
+    my $mtime = -e $vjs ? sprintf "%d", 24 * 60 * -M $vjs : "?";
+    printf "%d %d %s %s\n", scalar @versions, scalar @deps, $mtime, $crate;
 }
