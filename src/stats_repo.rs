36c90472adc58bedb75576df9b97ce5b1cad1b0e
Kent Fredric <kentnl@gentoo.org>
2020-11-10 09:49:10 +1300

    stats_repo: streamline error checking

--- src/stats_repo.rs
+++ src/stats_repo.rs
@@ -1,12 +1,19 @@
-use std::path::PathBuf;
+use std::{
+  ffi::OsString,
+  path::{Path, PathBuf},
+};
 
 #[derive(Debug)]
 pub enum Error {
-  RootNotExists(PathBuf),
   RootNotDir(PathBuf),
-  FileNotExists(PathBuf),
+  RootIoError(PathBuf, std::io::Error),
+  CrateNotDir(PathBuf),
+  CrateIoError(PathBuf, std::io::Error),
   FileNotReadable(PathBuf),
+  FileIoError(PathBuf, std::io::Error),
+  NotUnicode(OsString),
   IoError(std::io::Error),
+  VersionsError(super::versions::Error),
 }
 
 pub struct StatsRepo {
@@ -17,39 +24,87 @@ pub fn from_config(c: super::config::Config) -> StatsRepo { StatsRepo { root: c.
 
 impl StatsRepo {
   pub fn root(&self) -> Result<PathBuf, Error> {
-    if !self.root.exists() {
-      return Err(Error::RootNotExists(self.root.to_owned()));
-    }
-    if !self.root.is_dir() {
+    let meta = self.root.metadata().map_err(|e| Error::RootIoError(self.root.to_owned(), e))?;
+    if !meta.is_dir() {
       return Err(Error::RootNotDir(self.root.to_owned()));
     }
     Ok(self.root.to_owned())
   }
 
-  pub fn crates(&self) -> Result<Vec<String>, Error> { todo!() }
-
-  pub fn crate_versions_path<C>(&self, my_crate: C) -> Result<PathBuf, Error>
-  where
-    C: AsRef<str>,
-  {
-    let path = self.root()?.join(my_crate.as_ref()).join("versions.json");
-    if !path.exists() {
-      return Err(Error::FileNotExists(path));
+  pub fn crate_names(&self) -> Result<Vec<OsString>, Error> {
+    let mut x = Vec::new();
+    for entry in std::fs::read_dir(self.root()?)? {
+      let direntry = entry?;
+      let ent = direntry.file_name();
+      let spth = ent.to_owned().into_string().map_err(Error::NotUnicode)?;
+      if spth.starts_with('.') {
+        continue;
+      }
+      if direntry.file_type()?.is_dir() {
+        x.push(ent)
+      }
     }
-    if path.is_dir() {
+    Ok(x)
+  }
+
+  pub fn crate_path<C>(&self, my_crate: C) -> Result<PathBuf, Error>
+  where
+    C: AsRef<Path>,
+  {
+    let path = self.root()?.join(my_crate.as_ref());
+    let meta = path.metadata().map_err(|e| Error::CrateIoError(path.to_owned(), e))?;
+    if !meta.is_dir() {
+      return Err(Error::CrateNotDir(path));
+    }
+    Ok(path)
+  }
+
+  pub fn crate_file_path<C, F>(&self, my_crate: C, file: F) -> Result<PathBuf, Error>
+  where
+    C: AsRef<Path>,
+    F: AsRef<Path>,
+  {
+    let path = self.crate_path(my_crate)?.join(file.as_ref());
+    let meta = path.metadata().map_err(|e| Error::FileIoError(path.to_owned(), e))?;
+    if meta.is_dir() {
       return Err(Error::FileNotReadable(path));
     }
     Ok(path)
   }
 
-  pub fn crate_versions<C>(&self, _my_crate: C) -> Result<Vec<String>, Error>
+  pub fn crate_versions_path<C>(&self, my_crate: C) -> Result<PathBuf, Error>
   where
-    C: AsRef<str>,
+    C: AsRef<Path>,
   {
-    todo!()
+    self.crate_file_path(my_crate, "versions.json")
+  }
+
+  pub fn crate_results_path<C>(&self, my_crate: C) -> Result<PathBuf, Error>
+  where
+    C: AsRef<Path>,
+  {
+    self.crate_file_path(my_crate, "results.json")
+  }
+
+  pub fn crate_index_path<C>(&self, my_crate: C) -> Result<PathBuf, Error>
+  where
+    C: AsRef<Path>,
+  {
+    self.crate_file_path(my_crate, "index.html")
+  }
+
+  pub fn crate_versions<C>(&self, my_crate: C) -> Result<super::versions::VersionList, Error>
+  where
+    C: AsRef<Path>,
+  {
+    Ok(super::versions::from_file(self.crate_versions_path(my_crate)?)?)
   }
 }
 
 impl From<std::io::Error> for Error {
   fn from(e: std::io::Error) -> Self { Self::IoError(e) }
+}
+
+impl From<super::versions::Error> for Error {
+  fn from(e: super::versions::Error) -> Self { Self::VersionsError(e) }
 }
