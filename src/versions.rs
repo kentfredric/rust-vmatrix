60bf28bbe6abcaf38596b578107c0bb8e2df2c64
Kent Fredric <kentnl@gentoo.org>
2020-11-11 13:44:59 +1300

    versions: squash error into calling scope

--- src/versions.rs
+++ src/versions.rs
@@ -1,18 +1,16 @@
-mod err;
 mod version_list;
 
-pub use err::Error;
 use std::path::PathBuf;
 pub use version_list::VersionList;
 
-pub fn from_str<N>(content: N) -> Result<VersionList, Error>
+pub fn from_str<N>(content: N) -> Result<VersionList, self::Error>
 where
   N: AsRef<str>,
 {
   use serde_json::from_str;
   Ok(from_str(content.as_ref())?)
 }
-pub fn from_file<N>(file: N) -> Result<VersionList, Error>
+pub fn from_file<N>(file: N) -> Result<VersionList, self::Error>
 where
   N: Into<PathBuf>,
 {
@@ -23,4 +21,17 @@ where
   let mut contents = String::new();
   file.read_to_string(&mut contents)?;
   from_str(contents)
+}
+
+#[derive(Debug)]
+pub enum Error {
+  IoError(std::io::Error),
+  SerdeJsonError(serde_json::Error),
+}
+
+impl From<std::io::Error> for Error {
+  fn from(e: std::io::Error) -> Self { Self::IoError(e) }
+}
+impl From<serde_json::Error> for Error {
+  fn from(e: serde_json::Error) -> Self { Self::SerdeJsonError(e) }
 }
