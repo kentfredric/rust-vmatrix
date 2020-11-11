6fee2208b76317924f0485a059279f57f383de9a
Kent Fredric <kentnl@gentoo.org>
2020-11-11 13:31:04 +1300

    config: Flatten Error class into call context

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,10 @@
 use std::path::PathBuf;
 
 mod config_data;
-mod err;
-pub use self::{config_data::Config, err::Error};
 
-pub fn from_str<N>(content: N) -> Result<Config, Error>
+pub use self::config_data::Config;
+
+pub fn from_str<N>(content: N) -> Result<Config, self::Error>
 where
   N: AsRef<str>,
 {
@@ -12,7 +12,7 @@ where
   Ok(from_str(content.as_ref())?)
 }
 
-pub fn from_file<N>(file: N) -> Result<Config, Error>
+pub fn from_file<N>(file: N) -> Result<Config, self::Error>
 where
   N: Into<PathBuf>,
 {
@@ -24,4 +24,18 @@ where
 
   file.read_to_string(&mut contents)?;
   from_str(contents)
+}
+
+#[derive(Debug)]
+pub enum Error {
+  TomlDecodeError(toml::de::Error),
+  IoError(std::io::Error),
+}
+
+impl From<toml::de::Error> for Error {
+  fn from(e: toml::de::Error) -> Self { Self::TomlDecodeError(e) }
+}
+
+impl From<std::io::Error> for Error {
+  fn from(e: std::io::Error) -> Self { Self::IoError(e) }
 }
