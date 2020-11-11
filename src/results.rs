48570ec87bc78b74e06a55542b0cf24b40a4ed00
Kent Fredric <kentnl@gentoo.org>
2020-11-11 13:34:37 +1300

    results: squash error into local calling scope

--- src/results.rs
+++ src/results.rs
@@ -1,20 +1,19 @@
-mod err;
 mod result_info;
 mod result_list;
 mod result_type;
 
 use std::path::PathBuf;
 
-pub use self::{err::Error, result_info::ResultInfo, result_list::ResultList, result_type::ResultType};
+pub use self::{result_info::ResultInfo, result_list::ResultList, result_type::ResultType};
 
-pub fn from_str<N>(content: N) -> Result<ResultList, Error>
+pub fn from_str<N>(content: N) -> Result<ResultList, self::Error>
 where
   N: AsRef<str>,
 {
   use serde_json::from_str;
   Ok(from_str(content.as_ref())?)
 }
-pub fn from_file<N>(file: N) -> Result<ResultList, Error>
+pub fn from_file<N>(file: N) -> Result<ResultList, self::Error>
 where
   N: Into<PathBuf>,
 {
@@ -25,4 +24,17 @@ where
   let mut contents = String::new();
   file.read_to_string(&mut contents)?;
   from_str(contents)
+}
+
+#[derive(Debug)]
+pub enum Error {
+  IoError(std::io::Error),
+  SerdeJsonError(serde_json::Error),
+}
+
+impl From<std::io::Error> for Error {
+  fn from(e: std::io::Error) -> Self { Self::IoError(e) }
+}
+impl From<serde_json::Error> for Error {
+  fn from(e: serde_json::Error) -> Self { Self::SerdeJsonError(e) }
 }
