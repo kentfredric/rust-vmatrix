b03d04b275890d3de856d6b7fe6f27ab1fcca026
Kent Fredric <kentnl@gentoo.org>
2020-11-11 15:47:33 +1300

    Untested_targets: More projection tuning and rate recording

--- untested_targets.pl
+++ untested_targets.pl
@@ -8,7 +8,7 @@ use resultdb;
 
 my $n_crates    = 0;
 my $n_targets   = 0;
-my $e_realistic = 7.58;
+my $e_realistic = 3.93;
 my $e_huge      = 60;
 
 my $rdb = resultdb->new();
@@ -26,21 +26,51 @@ for my $crate ( $rdb->crate_names ) {
     }
 }
 if ( $ENV{SUMMARY} ) {
+    my $global_reference         = 1604701775;
+    my $global_reference_targets = 1646374;
     require JSON::PrettyCompact;
+    my $file = $rdb->root() . '/projections.json';
+    my $prev_report;
+    if ( -e $file ) {
+        require JSON::MaybeXS;
+        open my $fh, "<:utf8", $file or die "can't read $file, $!";
+        local $/;
+        $prev_report = JSON::MaybeXS->new->decode( scalar <$fh> );
+    }
     my $pc     = JSON::PrettyCompact->new();
     my $report = {
-        at       => scalar gmtime(),
-        crates   => $n_crates,
-        targets  => $n_targets,
-        est_low  => num_fmt( $n_targets * $e_realistic ),
-        est_high => num_fmt( $n_targets * $e_huge ),
+        at          => scalar gmtime(),
+        at_unixtime => scalar time(),
+        crates      => $n_crates,
+        targets     => $n_targets,
+        est_low     => num_fmt( $n_targets * $e_realistic ),
+        est_high    => num_fmt( $n_targets * $e_huge ),
     };
+    if ($prev_report) {
+        if ( $prev_report->{at_unixtime} ) {
+            my $elapsed  = $report->{at_unixtime} - $prev_report->{at_unixtime};
+            my $target_d = $prev_report->{targets} - $report->{targets};
+            if ( $target_d > 0 ) {
+                $report->{time_per_target} = $elapsed / $target_d;
+            }
+        }
+    }
+    {
+        my $global_elapsed  = $report->{at_unixtime} - $global_reference;
+        my $global_target_d = $global_reference_targets - $report->{targets};
+        if ( $global_target_d > 0 ) {
+            $report->{global_time_per_target} =
+              $global_elapsed / $global_target_d;
+        }
+    }
     my $ds = $pc->encode($report);
-    printf "At %s GMT\n",              $report->{at};
-    printf "%d crates\n",              $report->{crates};
-    printf "%d targets\n",             $report->{targets};
-    printf "Realistic low time: %s\n", $report->{est_low};
-    printf "Bad high time: %s\n",      $report->{est_high};
+    printf "At %s GMT\n",               $report->{at};
+    printf "%d crates\n",               $report->{crates};
+    printf "%d targets\n",              $report->{targets};
+    printf "Realistic low time: %s\n",  $report->{est_low};
+    printf "Bad high time: %s\n",       $report->{est_high};
+    printf "Rate: %8.2f secs/target\n", $report->{time_per_target}
+      if exists $report->{time_per_target};
     my $dest = $rdb->root() . '/projections.json';
     open my $fh, ">:utf8", $dest or die "can't write $dest, $!";
     $fh->print($ds);
