cb39c439ab4804c29e388eb6e72d93eb406dc464
Kent Fredric <kentnl@gentoo.org>
2020-11-23 04:41:21 +1300

    ::Error : use thiserror

--- src/lib.rs
+++ src/lib.rs
@@ -5,38 +5,14 @@ pub mod stats_repo;
 pub mod stats_repo_cache;
 pub mod versions;
 
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum Error {
-  ConfigError(config::Error),
-  ResultsError(results::Error),
-  VersionsError(versions::Error),
-  StatsRepoError(stats_repo::Error),
+  #[error("Configuration Error: {0}")]
+  ConfigError(#[from] config::Error),
+  #[error("Result Data Error: {0}")]
+  ResultsError(#[from] results::Error),
+  #[error("Version Data Error: {0}")]
+  VersionsError(#[from] versions::Error),
+  #[error("Error in Stats Repo: {0}")]
+  StatsRepoError(#[from] stats_repo::Error),
 }
-
-impl From<config::Error> for Error {
-  fn from(e: config::Error) -> Self { Self::ConfigError(e) }
-}
-
-impl From<results::Error> for Error {
-  fn from(e: results::Error) -> Self { Self::ResultsError(e) }
-}
-
-impl From<stats_repo::Error> for Error {
-  fn from(e: stats_repo::Error) -> Self { Self::StatsRepoError(e) }
-}
-
-impl From<versions::Error> for Error {
-  fn from(e: versions::Error) -> Self { Self::VersionsError(e) }
-}
-
-impl std::fmt::Display for Error {
-  fn fmt(&self, fmter: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-    match self {
-      | Self::ConfigError(e) => write!(fmter, "Configuration Error: {}", e),
-      | Self::ResultsError(e) => write!(fmter, "Result Data Error: {}", e),
-      | Self::StatsRepoError(e) => write!(fmter, "Error in Stats Repo: {}", e),
-      | Self::VersionsError(e) => write!(fmter, "Version Data Error: {}", e),
-    }
-  }
-}
-impl std::error::Error for Error {}
