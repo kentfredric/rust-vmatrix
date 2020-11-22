7f76578331613377bfb627cd537b2de4ef6a81f7
Kent Fredric <kentnl@gentoo.org>
2020-11-23 04:01:14 +1300

    config::Error : use thiserror

--- src/config.rs
+++ src/config.rs
@@ -26,30 +26,10 @@ where
   from_str(contents)
 }
 
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum Error {
-  TomlDecodeError(toml::de::Error),
-  IoError(std::io::Error),
+  #[error("Error reading TOML data: {0}")]
+  TomlDecodeError(#[from] toml::de::Error),
+  #[error("Error reading Config TOML file: {0}")]
+  IoError(#[from] std::io::Error),
 }
-
-impl From<toml::de::Error> for Error {
-  fn from(e: toml::de::Error) -> Self { Self::TomlDecodeError(e) }
-}
-
-impl From<std::io::Error> for Error {
-  fn from(e: std::io::Error) -> Self { Self::IoError(e) }
-}
-
-impl std::fmt::Display for Error {
-  fn fmt(&self, fmter: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-    match self {
-      | Self::TomlDecodeError(e) => {
-        write!(fmter, "Error reading TOML data: {}", e)
-      },
-      | Self::IoError(e) => {
-        write!(fmter, "Error reading Config TOML file: {}", e)
-      },
-    }
-  }
-}
-impl std::error::Error for Error {}
