a6c150a8a8dedf4adaff3b6c8682770a43ae1c34
Kent Fredric <kentnl@gentoo.org>
2020-11-26 03:10:31 +1300

    Document crate root in a basic way

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+//! A collection of tools for managing testing of multiple versions of rust crates against multiple rusts
+
 mod config;
 mod crate_dir;
 
@@ -13,16 +15,23 @@ pub use stats_repo::{StatsRepo, StatsRepoError};
 pub use stats_repo_cache::{StatsRepoCache, StatsRepoCacheError};
 pub use version_list::{VersionInfo, VersionList, VersionsError};
 
+
+/// Global Collector Error type for all internal error kinds
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
+  /// Errors from [`Config`]
   #[error("Configuration Error: {0}")]
   ConfigError(#[from] ConfigError),
+  /// Errors from [`ResultList`] and friends
   #[error("Result Data Error: {0}")]
   ResultsError(#[from] ResultsError),
+  /// Errors from [`VersionList`] and friends
   #[error("Version Data Error: {0}")]
   VersionsError(#[from] VersionsError),
+  /// Errors from [`StatsRepo`]
   #[error("Error in Stats Repo: {0}")]
   StatsRepoError(#[from] StatsRepoError),
+  /// Errors from [`StatsRepoCache`]
   #[error("Error in Stats Repo Cache: {0}")]
   StatsRepoCacheError(#[from] StatsRepoCacheError),
 }
