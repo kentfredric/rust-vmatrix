7ec10dfa1e6754e720c59e346ef17544d6d55669
Kent Fredric <kentnl@gentoo.org>
2020-12-02 01:41:21 +1300

    flesh out load_config example

--- examples/load_config.rs
+++ examples/load_config.rs
@@ -1,6 +1,30 @@
-fn main() {
-  use std::path::Path;
-  let cfg = vmatrix::Config::from_path(Path::new("./vmatrix.toml")).unwrap();
+type EResult = Result<(), Box<dyn std::error::Error>>;
 
-  dbg!(cfg.rustc().unwrap());
+fn from_file() -> EResult {
+  use std::path::Path;
+  let cfg = vmatrix::Config::from_path(Path::new("./vmatrix.toml"))?;
+  eprintln!("{:#?}", cfg);
+  eprintln!("root() {:#?}", cfg.root());
+  eprintln!("rustc() {:#?}", cfg.rustc());
+  Ok(())
+}
+fn from_str() -> EResult {
+  let cfg: vmatrix::Config = r#"
+    [stats_repo]
+    root = "/home/kent/rust/vmatrix/"
+
+    [targets]
+    rustc = [ "1.46.0", "1.47.0" ]
+  "#
+  .parse()?;
+  eprintln!("{:#?}", cfg);
+  eprintln!("root() {:#?}", cfg.root());
+  eprintln!("rustc() {:#?}", cfg.rustc());
+  Ok(())
+}
+
+fn main() -> EResult {
+  from_file()?;
+  from_str()?;
+  Ok(())
 }
